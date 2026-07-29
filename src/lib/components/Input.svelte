e585f229998e1d01d6670e814d4ee80ce28c2bbe
EcvServer-cloud <evercaballero4@gmail.com>
2026-07-28 19:22:51 -0500

    Refactor Input component to support more types

--- src/lib/components/Input.svelte
+++ src/lib/components/Input.svelte
@@ -1,40 +1,97 @@
 <script>
-	export let label = '';
-	export let value = '';
-	export let type = 'text';
-	export let placeholder = '';
-	export let required = false;
-	export let hint = '';
-	export let id = label ? label.toLowerCase().replace(/\s+/g, '-') : crypto.randomUUID();
-	export let step = undefined;
-	export let min = undefined;
+  export let id = '';
+  export let label = '';
+  export let type = 'text';
+  export let value = '';
+  export let placeholder = '';
+  export let required = false;
+  export let step = undefined;
+  export let min = undefined;
+  export let max = undefined;
+  export let disabled = false;
 </script>
 
-<div>
-	{#if label}
-		<label class="field-label" for={id}>{label}{#if required}<span class="text-heart"> *</span>{/if}</label>
-	{/if}
-	{#if type === 'textarea'}
-		<textarea
-			{id}
-			class="field-input min-h-[92px] resize-y"
-			bind:value
-			{placeholder}
-			{required}
-		/>
-	{:else}
-		<input
-			{id}
-			class="field-input"
-			{type}
-			{step}
-			{min}
-			bind:value
-			{placeholder}
-			{required}
-		/>
-	{/if}
-	{#if hint}
-		<p class="mt-1 text-xs text-muted">{hint}</p>
-	{/if}
+<div class="field">
+  {#if label}
+    <label for={id} class="field-label">
+      {label}
+      {#if required}
+        <span class="text-red-500">*</span>
+      {/if}
+    </label>
+  {/if}
+
+  {#if type === 'number'}
+    <input
+      {id}
+      class="field-input"
+      type="number"
+      bind:value
+      {placeholder}
+      {required}
+      {disabled}
+      {step}
+      {min}
+      {max}
+    />
+  {:else if type === 'email'}
+    <input
+      {id}
+      class="field-input"
+      type="email"
+      bind:value
+      {placeholder}
+      {required}
+      {disabled}
+    />
+  {:else if type === 'tel'}
+    <input
+      {id}
+      class="field-input"
+      type="tel"
+      bind:value
+      {placeholder}
+      {required}
+      {disabled}
+    />
+  {:else if type === 'date'}
+    <input
+      {id}
+      class="field-input"
+      type="date"
+      bind:value
+      {required}
+      {disabled}
+    />
+  {:else if type === 'url'}
+    <input
+      {id}
+      class="field-input"
+      type="url"
+      bind:value
+      {placeholder}
+      {required}
+      {disabled}
+    />
+  {:else if type === 'password'}
+    <input
+      {id}
+      class="field-input"
+      type="password"
+      bind:value
+      {placeholder}
+      {required}
+      {disabled}
+    />
+  {:else}
+    <input
+      {id}
+      class="field-input"
+      type="text"
+      bind:value
+      {placeholder}
+      {required}
+      {disabled}
+    />
+  {/if}
 </div>
